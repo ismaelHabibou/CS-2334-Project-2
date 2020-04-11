a3bb701f6f8eff933fb3285f5517e7af0aae4f57
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-10 21:27:38 -0500

    I have created necessary objects that will be used for testing

--- src/Tests/CircularBoundaryConditionsTest.java
+++ src/Tests/CircularBoundaryConditionsTest.java
@@ -3,13 +3,23 @@ package Tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import projects.CircularBoundaryConditions;
+import projects.Generation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircularBoundaryConditionsTest {
+ /** Create an instance of circular boundary conditions*/
+    private CircularBoundaryConditions boundaryConditions;
+
+ /** Create a generation*/
+    private Generation generation;
 
     @BeforeEach
     void setUp() {
+       String state = "O O O O O O O O O O O O O O O O O ";
+
+       boundaryConditions = new CircularBoundaryConditions();
     }
 
     @AfterEach
