78b9846e87ea2ae286d859a1e2b585e16f814f5d
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-11 18:35:20 -0500

    Destruction of ressources created for unit Testing

--- src/Tests/FixedBoundaryConditionsTest.java
+++ src/Tests/FixedBoundaryConditionsTest.java
@@ -31,6 +31,10 @@ class FixedBoundaryConditionsTest {
 
     @AfterEach
     void tearDown() {
+        leftState = null;
+        rightState = null;
+        generation = null;
+        boundaryConditions = null;
     }
 
     @Test
