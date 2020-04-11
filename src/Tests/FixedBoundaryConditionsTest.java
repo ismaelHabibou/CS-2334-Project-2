d324d1fe2366cce5ac2350561cd2f4471ee4058e
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-11 18:11:13 -0500

    Ressources for testing FixedBoundaryConditions class

--- src/Tests/FixedBoundaryConditionsTest.java
+++ src/Tests/FixedBoundaryConditionsTest.java
@@ -3,13 +3,25 @@ package Tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import projects.CellState;
+import projects.FixedBoundaryConditions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FixedBoundaryConditionsTest {
+ /** declare two CellStates*/
+    CellState leftState;
+    CellState rightState;
+
+    /** declare  a fixed boundary conditions*/
+    FixedBoundaryConditions boundaryConditions;
 
     @BeforeEach
     void setUp() {
+        leftState = CellState.OFF;
+        rightState = CellState.ON;
+
+        boundaryConditions = new FixedBoundaryConditions(leftState, rightState);
     }
 
     @AfterEach
