12c3cb158044443cd187bb11c67294c54653724e
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-11 20:37:06 -0500

    Test case for getNeighbor()

--- src/Tests/ElementaryRuleTest.java
+++ src/Tests/ElementaryRuleTest.java
@@ -39,6 +39,25 @@ class ElementaryRuleTest {
 
     @Test
     void getNeighborhood() {
+        int index = 3;
+        // For circular boundary conditions
+        Cell[] neighborhoodForCellAt0 =
+                {generation.getCell(generation.size() - 1),generation.getCell(0),generation.getCell(1)};
+        Cell[] neighborhoodForCellAtEnd = {generation.getCell(generation.size() - 2), generation.getCell(generation.size() - 1),generation.getCell(0)};
+        Cell[] neighborhoodForCellAtIndex = {generation.getCell(index - 1), generation.getCell(index),generation.getCell(index + 1)};
+
+        assertArrayEquals(neighborhoodForCellAt0,rule.getNeighborhood(0,generation,circular));
+        assertArrayEquals(neighborhoodForCellAtEnd,rule.getNeighborhood(generation.size() - 1,generation,circular));
+        assertArrayEquals(neighborhoodForCellAtIndex,rule.getNeighborhood(index,generation, circular));
+
+        // For fixed boundary conditions
+        neighborhoodForCellAt0 = new Cell[]{fixed.getNeighbor(0, -1, generation), generation.getCell(0), generation.getCell(1)};
+        neighborhoodForCellAtEnd = new Cell[]{generation.getCell(generation.size() - 2), generation.getCell(generation.size() - 1),fixed.getNeighbor(generation.size() - 1,1,generation)};
+        neighborhoodForCellAtIndex = new Cell[]{fixed.getNeighbor(index,-1,generation),fixed.getNeighbor(index,0,generation),fixed.getNeighbor(index, 1, generation)};
+
+        assertArrayEquals(neighborhoodForCellAt0,rule.getNeighborhood(0,generation,fixed));
+        assertArrayEquals(neighborhoodForCellAtEnd,rule.getNeighborhood(generation.size() - 1,generation,fixed));
+        assertArrayEquals(neighborhoodForCellAtIndex,rule.getNeighborhood(index,generation, fixed));
     }
 
     @Test
