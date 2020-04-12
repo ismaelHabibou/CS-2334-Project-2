f911950e3e502b46a3360da2e338e2c13bfd2935
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-11 23:31:50 -0500

    Test for the toString() method

--- src/Tests/ElementaryRuleTest.java
+++ src/Tests/ElementaryRuleTest.java
@@ -3,7 +3,12 @@ package Tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import projects.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,13 +18,13 @@ class ElementaryRuleTest {
     Rule rule = new ElementaryRule(ruleNumber);
 
     /** Create a generation for the rule*/
-    Generation generation ;
+    static Generation generation ;
 
     /** Create boundary conditions*/
-    BoundaryConditions circular = new CircularBoundaryConditions();
+    static BoundaryConditions circular = new CircularBoundaryConditions();
 
     /** Create a fixed boundary conditions*/
-    BoundaryConditions fixed = new FixedBoundaryConditions(CellState.OFF,CellState.ON);
+    static BoundaryConditions fixed = new FixedBoundaryConditions(CellState.OFF,CellState.ON);
 
     @BeforeEach
     void setUp() {
@@ -29,6 +34,7 @@ class ElementaryRuleTest {
 
     @AfterEach
     void tearDown() {
+     generation = null;
     }
 
     @Test
@@ -37,32 +43,38 @@ class ElementaryRuleTest {
      assertEquals(numberSubRules,rule.getNumSubrules());
     }
 
-    @Test
-    void getNeighborhood() {
-        int index = 3;
-        // For circular boundary conditions
-        Cell[] neighborhoodForCellAt0 =
-                {generation.getCell(generation.size() - 1),generation.getCell(0),generation.getCell(1)};
-        Cell[] neighborhoodForCellAtEnd = {generation.getCell(generation.size() - 2), generation.getCell(generation.size() - 1),generation.getCell(0)};
-        Cell[] neighborhoodForCellAtIndex = {generation.getCell(index - 1), generation.getCell(index),generation.getCell(index + 1)};
+     @ParameterizedTest
+    @MethodSource("arrayStream1")
+    void getNeighborhoodWithCircularBoundary(Object[][] myObject) {
+        int index = (Integer) myObject[0][0];
+        Cell[] cells = (Cell[]) myObject[1];
 
-        assertArrayEquals(neighborhoodForCellAt0,rule.getNeighborhood(0,generation,circular));
-        assertArrayEquals(neighborhoodForCellAtEnd,rule.getNeighborhood(generation.size() - 1,generation,circular));
-        assertArrayEquals(neighborhoodForCellAtIndex,rule.getNeighborhood(index,generation, circular));
+        assertArrayEquals(cells, rule.getNeighborhood(index,generation,circular));
 
-        // For fixed boundary conditions
-        neighborhoodForCellAt0 = new Cell[]{fixed.getNeighbor(0, -1, generation), generation.getCell(0), generation.getCell(1)};
-        neighborhoodForCellAtEnd = new Cell[]{generation.getCell(generation.size() - 2), generation.getCell(generation.size() - 1),fixed.getNeighbor(generation.size() - 1,1,generation)};
-        neighborhoodForCellAtIndex = new Cell[]{fixed.getNeighbor(index,-1,generation),fixed.getNeighbor(index,0,generation),fixed.getNeighbor(index, 1, generation)};
+    }
 
-        assertArrayEquals(neighborhoodForCellAt0,rule.getNeighborhood(0,generation,fixed));
-        assertArrayEquals(neighborhoodForCellAtEnd,rule.getNeighborhood(generation.size() - 1,generation,fixed));
-        assertArrayEquals(neighborhoodForCellAtIndex,rule.getNeighborhood(index,generation, fixed));
+    @ParameterizedTest
+    @MethodSource("arrayStream2")
+    void getNeighborhoodWithFixedBoundary(Object[][] myObject) {
+        int index = (Integer) myObject[0][0];
+        Cell[] cells = (Cell[]) myObject[1];
+
+        assertArrayEquals(cells, rule.getNeighborhood(index,generation,fixed));
+
     }
 
     @Test
     void testToString() {
+      String on = CellState.ON.toString();
+      String off = CellState.OFF.toString();
+
+        String ruleString = on + on + on + " " + on + on + off + " " + on + off + on + " " + on + off + off + " " +
+                off + on + on + " " + off + on + off + " " + off + off + on + " " + off + off + off + "\n" + ruleToString(ruleNumber);
+
+        assertEquals(ruleString,rule.toString());
+
     }
+
 
     @ParameterizedTest
     @MethodSource("arrayStream")
@@ -84,5 +96,50 @@ class ElementaryRuleTest {
                 Arguments.arguments((Object) new Object[][]{{new EvolvedCell(CellState.ON, 1)}, {new Cell(CellState.OFF), new Cell(CellState.OFF), new Cell(CellState.ON)}}),
                 Arguments.arguments((Object) new Object[][]{{new EvolvedCell(CellState.OFF, 0)}, {new Cell(CellState.OFF), new Cell(CellState.OFF), new Cell(CellState.OFF)}})
         );
+    }
+
+ /** Parametrize test for circular boundary condition*/
+     static Stream<Arguments> arrayStream1(){
+
+        return Stream.of(Arguments.arguments( new Object[][]{{0}, {circular.getNeighbor(0,-1 , generation),
+                        circular.getNeighbor(0,0,generation),circular.getNeighbor(0,1,generation)}}),
+                Arguments.arguments( new Object[][]{ {generation.size() - 1}, {circular.getNeighbor(generation.size() - 1,-1 , generation),
+                        circular.getNeighbor(generation.size() - 1,0,generation),circular.getNeighbor(generation.size() - 1,1,generation)}}),
+                Arguments.arguments( new Object[][]{{3}, {circular.getNeighbor(3,-1 , generation),circular.getNeighbor(3,0,generation),
+                        circular.getNeighbor(3,1,generation)}})
+        );
+
+    }
+
+    /** Parametrize test for fixed boundary conditions*/
+    static Stream<Arguments> arrayStream2(){
+        return Stream.of(Arguments.arguments(new Object[][]{{0}, {fixed.getNeighbor(0, -1, generation),
+                        fixed.getNeighbor(0, 0, generation),fixed.getNeighbor(0, 1, generation)}}),
+                Arguments.arguments(new Object[][]{{generation.size() -1}, {fixed.getNeighbor(generation.size() - 1,-1,generation),
+                        fixed.getNeighbor(generation.size() - 1,0,generation),fixed.getNeighbor(generation.size() - 1,1,generation)}}),
+                Arguments.arguments(new Object[][]{{3}, {fixed.getNeighbor(3,-1,generation),fixed.getNeighbor(3,0,generation),
+                        fixed.getNeighbor(3, 1, generation)}}));
+    }
+
+ /** Convert rule to String*/
+    private static String ruleToString(int ruleNumber){
+        StringBuilder buffer = new StringBuilder();
+        String ruleNumString = addLeadingZero(Integer.toBinaryString(ruleNumber));
+
+        for (int i = 0; i < ruleNumString.length(); i++)
+            buffer.append(" ").append(ruleNumString.charAt(i) == '1'? CellState.ON.toString() : CellState.OFF.toString()).append(" ");
+
+        return buffer.toString();
+    }
+
+    private static String addLeadingZero(String binaryString) {
+        final int NUMBER_OF_BINARY_DIGIT = 8;
+        StringBuilder buffer = new StringBuilder();
+
+        for (int i = 0; i < NUMBER_OF_BINARY_DIGIT - binaryString.length(); i++)
+            buffer.append("0");
+        buffer.append(binaryString);
+
+        return buffer.toString();
     }
 }
