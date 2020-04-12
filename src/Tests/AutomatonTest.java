f61597a94990dd57ae290405b7f432eaead978e0
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-12 17:42:41 -0500

    Ressources for automaton test class

--- src/Tests/AutomatonTest.java
+++ src/Tests/AutomatonTest.java
@@ -3,8 +3,23 @@ package Tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import projects.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class AutomatonTest {
+    private int ruleNum = 22;
+    /** Generations*/
+    Generation gen2 = new Generation("   OOOOO   ");
+
+    /** Rules*/
+    Rule rule1 = new ElementaryRule(ruleNum);
+
+    /** Boundary conditons*/
+    BoundaryConditions conditions = new CircularBoundaryConditions();
+
+    /** Automatons*/
+    Automaton automaton = new Automaton(rule1,gen2,conditions);
 
     @BeforeEach
     void setUp() {
