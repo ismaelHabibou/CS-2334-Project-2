7467108d900c1f919f7a66bb03cb2e0c1330f794
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-12 19:28:56 -0500

    Necessary ressources for Testing AutomatonMeasurements

--- src/Tests/AutomatonMeasurementsTest.java
+++ src/Tests/AutomatonMeasurementsTest.java
@@ -5,6 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutomatonMeasurementsTest {
+ /** Create necessary resources */
+    Rule rule = new ElementaryRule(22);
+    BoundaryConditions circular = new CircularBoundaryConditions();
+    Generation firstGenerations = new Generation("       O       ");
+
+    Automaton automaton = new Automaton(rule,firstGenerations,circular);
+
+    AutomatonMeasurementsTest() throws InvalidRuleNumException {
+    }
 
     @Test
     void hammingDistance() {
