3cb3a35051e4c8d4d9e16921366e0b312ee633a7
ismaelHabibou <habibouissakaismael2@ou.edu>
2020-04-12 23:01:22 -0500

    Comments for InvalidRuleNumException class

--- src/projects/InvalidRuleNumException.java
+++ src/projects/InvalidRuleNumException.java
@@ -3,7 +3,9 @@ package projects;
 public class InvalidRuleNumException extends Exception {
     private static final long serialVersionUID = 1L;
 
-    /** Create an exception*/
+    /** Create an exception
+     * @param ruleNum The rule number
+     * */
     public InvalidRuleNumException(int ruleNum){
         super("Invalid rule: " + ruleNum);
     }
